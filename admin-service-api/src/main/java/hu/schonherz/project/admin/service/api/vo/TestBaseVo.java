acb540ced7e9522caafd3b43cb228d1f4152ecca
gfazek <fg.gergo@gmail.com>
2017-01-26 00:16:16 +0100

    vo classes use lombok, parent pom corrected

--- admin-service-api/src/main/java/hu/schonherz/project/admin/service/api/vo/TestBaseVo.java
+++ admin-service-api/src/main/java/hu/schonherz/project/admin/service/api/vo/TestBaseVo.java
@@ -2,15 +2,16 @@ package hu.schonherz.project.admin.service.api.vo;
 
 import java.io.Serializable;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class TestBaseVo implements Serializable{
 
 	private static final long serialVersionUID = -3097333328363375029L;
 
 	private Long id;
-
-	public TestBaseVo() {
-		super();
-	}
 
 	public Long getId() {
 		return id;
--- admin-service-api/src/main/java/hu/schonherz/project/admin/service/api/vo/TestUserVo.java
+++ admin-service-api/src/main/java/hu/schonherz/project/admin/service/api/vo/TestUserVo.java
@@ -2,43 +2,17 @@ package hu.schonherz.project.admin.service.api.vo;
 
 import java.io.Serializable;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class TestUserVo extends TestBaseVo implements Serializable{
 	
 	private static final long serialVersionUID = 5534955718398254750L;
 
 	private String username;
-
 	private String password;
-	
 	private String email;
 
-	public TestUserVo() {
-		super();
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
 }
