d17f7c308c933c9234b0d41b1ff87842e82823c1
gfazek <fg.gergo@gmail.com>
2017-01-28 17:09:18 +0100

    UserRepository created and initialized

---
+++ admin-data/src/main/java/hu/schonherz/project/admin/data/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package hu.schonherz.project.admin.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import hu.schonherz.project.admin.data.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long>{
+    
+    UserEntity findByUsername(String username);
+    
+}
