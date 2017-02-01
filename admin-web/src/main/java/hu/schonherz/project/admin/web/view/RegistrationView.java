d40ade4b1fa5c3adb31517d6aae3bc67832b0175
Joey <kovtamas1991@gmail.com>
2017-02-01 17:19:39 +0100

    Server side validation for the registration added.

--- admin-web/src/main/java/hu/schonherz/project/admin/web/view/RegistrationView.java
+++ admin-web/src/main/java/hu/schonherz/project/admin/web/view/RegistrationView.java
@@ -33,15 +33,41 @@ public class RegistrationView {
 
     public void registration() {
         FacesContext context = FacesContext.getCurrentInstance();
+        // Basic validation
+        if (!isUserDataValid()) {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Failed", "All fields must be filled, password must be at least 6 character long."));
+            LOG.info("User tried to register without filling all fields.");
+            return;
+        }
+
+        // Try to save user data. Notify the user about the result.
         try {
             userServiceRemote.registrationUser(userVo);
             context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success!", "Registration was successful."
                     + "Username or email already in use."));
+            LOG.info("User '{}' successfully registered.", userVo.getUsername());
         } catch (InvalidUserDataException iude) {
             context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Failed!", "Invalid user data. "
                     + "Username or email already in use."));
             LOG.warn("Unsuccessful registration attempt with data:{}{} ",
                     System.getProperty("line.separator"), userVo);
+            LOG.warn("Causing exception:" + System.getProperty("line.separator"), iude);
         }
+    }
+
+    private boolean isUserDataValid() {
+        if (hasEmptyUserData()) {
+            return false;
+        }
+
+        final int minPasswordLength = 6;
+        return userVo.getPassword().length() >= minPasswordLength;
+    }
+
+    private boolean hasEmptyUserData() {
+        return !(userVo.getEmail() != null && !userVo.getEmail().isEmpty()
+                && userVo.getUsername() != null && !userVo.getUsername().isEmpty()
+                && userVo.getPassword() != null && !userVo.getPassword().isEmpty());
     }
 }
