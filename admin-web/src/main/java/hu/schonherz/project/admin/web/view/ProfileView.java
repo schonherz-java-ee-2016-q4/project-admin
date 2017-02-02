1c21fbef8cbf81af26ae44fc0d5629fe5edde524
gfazek <fg.gergo@gmail.com>
2017-02-02 16:22:13 +0100

    profileview added

---
+++ admin-web/src/main/java/hu/schonherz/project/admin/web/view/ProfileView.java
@@ -0,0 +1,40 @@
+package hu.schonherz.project.admin.web.view;
+
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import hu.schonherz.project.admin.service.api.service.UserServiceRemote;
+import hu.schonherz.project.admin.service.api.service.exception.InvalidUserDataException;
+import hu.schonherz.project.admin.service.api.vo.UserVo;
+import lombok.Data;
+
+@ManagedBean(name = "profileView")
+@ViewScoped
+@Data
+public class ProfileView {
+    
+    private UserVo userVo;
+    
+    @EJB
+    private UserServiceRemote userServiceRemote;
+    
+    @PostConstruct
+    public void buildUser() {
+//        Mock username
+        final String username = "redsnake327";
+        userVo = userServiceRemote.findByUsername(username);
+    }
+    
+    public void save() {
+//        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            userServiceRemote.registrationUser(userVo);
+        } catch (InvalidUserDataException e) {
+          
+        }
+    }
+
+}
