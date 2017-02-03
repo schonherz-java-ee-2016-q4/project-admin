93e2d189cf42c587dfa3bba182d83628faf690ca
gfazek <fg.gergo@gmail.com>
2017-02-03 15:06:46 +0100

    profile validation

--- admin-web/src/main/java/hu/schonherz/project/admin/web/view/ProfileView.java
+++ admin-web/src/main/java/hu/schonherz/project/admin/web/view/ProfileView.java
@@ -2,12 +2,17 @@ package hu.schonherz.project.admin.web.view;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import hu.schonherz.project.admin.service.api.service.UserServiceRemote;
 import hu.schonherz.project.admin.service.api.service.exception.InvalidUserDataException;
 import hu.schonherz.project.admin.service.api.vo.UserVo;
+import hu.schonherz.project.admin.web.view.form.FormValidator;
+import hu.schonherz.project.admin.web.view.form.FormValidator.MessageBinding;
+import hu.schonherz.project.admin.web.view.form.ProfileForm;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,24 +22,73 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ProfileView {
 
-    private UserVo userVo;
-
+    private static final String SUCCESS = "Success";
+    private static final String FAILURE = "Failure";
+    private static final String SUCCESSFUL_CHANGING = "Your changes were successfully saved.";
+    private static final String DUPLICATION_EMAIL = "Invalid email data, because it's already in use!";
+    private static final String BASE_COMP_ID = "profileForm:";
+    private static final String EMAIL_COMP_ID = BASE_COMP_ID + "email";
+    private static final String PASSWORD_COMP_ID = BASE_COMP_ID + "currentPassword";
+    private static final String NEWPASSWORD_COMP_ID = BASE_COMP_ID + "newPassword";
+    private static final String GLOBAL_COMP_ID = "profileForm";
+    
+    private ProfileForm profileForm;
+    
     @EJB
     private UserServiceRemote userServiceRemote;
 
     @PostConstruct
-    public void buildUser() {
-//        Mock username
-        final String username = "redsnake327";
-        userVo = userServiceRemote.findByUsername(username);
+    public void init() {
+        profileForm = new ProfileForm();
+        UserVo userVo = userServiceRemote.findAll().get(0);
+        profileForm.setEmail(userVo.getEmail());
+        profileForm.setUsername(userVo.getUsername());
+        log.info(userVo.toString());
+        log.info(profileForm.toString());
     }
 
     public void save() {
-//        FacesContext context = FacesContext.getCurrentInstance();
-        try {
-            userServiceRemote.registrationUser(userVo);
-        } catch (InvalidUserDataException iude) {
-            log.warn("Could not update user " + userVo.getUsername(), iude);
+        FacesContext context = FacesContext.getCurrentInstance();
+        MessageBinding messageBinding = FormValidator.validateProfileForm(profileForm);
+        if (!messageBinding.isEmpty()) {
+            sendValidationMessages(messageBinding, context);
+            return;
         }
+        
+        try {
+            // Try to save user data
+            UserVo userVo = profileForm.getUserVo();
+            userServiceRemote.registrationUser(userVo);
+
+            // Notify user about success and log it
+            context.addMessage(GLOBAL_COMP_ID, new FacesMessage(FacesMessage.SEVERITY_INFO, SUCCESS, SUCCESSFUL_CHANGING));
+            log.info("User '{}' successfully changed his/her profile.", userVo.getUsername());
+        } catch (InvalidUserDataException iude) {
+            // Notify user about duplication and log it with details
+            context.addMessage(GLOBAL_COMP_ID, new FacesMessage(FacesMessage.SEVERITY_ERROR, FAILURE, DUPLICATION_EMAIL));
+            log.warn("Unsuccessful changing attempt with data:{}{} ", System.getProperty("line.separator"), profileForm);
+            log.warn("Causing exception:" + System.getProperty("line.separator"), iude);
+        }
+    }
+    
+    private void sendValidationMessages(MessageBinding messageBinding, FacesContext context) {
+        String message;
+        // Send e-mail validation message if there is one
+        if (messageBinding.hasMessageOfType(MessageBinding.MESSAGE_TYPES.EMAIL)) {
+            message = messageBinding.getMessage(MessageBinding.MESSAGE_TYPES.EMAIL);
+            context.addMessage(EMAIL_COMP_ID, new FacesMessage(FacesMessage.SEVERITY_ERROR, FAILURE, message));
+        }
+        
+        // Send password validation message if there is one
+        if (messageBinding.hasMessageOfType(MessageBinding.MESSAGE_TYPES.PASSWORD)) {
+            message = messageBinding.getMessage(MessageBinding.MESSAGE_TYPES.PASSWORD);
+            context.addMessage(PASSWORD_COMP_ID, new FacesMessage(FacesMessage.SEVERITY_ERROR, FAILURE, message));
+        }
+        //Send new password validation message if there is one
+        if (messageBinding.hasMessageOfType(MessageBinding.MESSAGE_TYPES.NEW_PASSWORD)) {
+            message = messageBinding.getMessage(MessageBinding.MESSAGE_TYPES.NEW_PASSWORD);
+            context.addMessage(NEWPASSWORD_COMP_ID, new FacesMessage(FacesMessage.SEVERITY_ERROR, FAILURE, message));
+        }
+        
     }
 }
--- admin-web/src/main/java/hu/schonherz/project/admin/web/view/form/ProfileForm.java
+++ admin-web/src/main/java/hu/schonherz/project/admin/web/view/form/ProfileForm.java
@@ -3,8 +3,10 @@ package hu.schonherz.project.admin.web.view.form;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
+@ToString
 public class ProfileForm extends UserForm {
 
     @Getter
