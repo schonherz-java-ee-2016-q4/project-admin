5c1dac201635b9c9bf92eb585ab543bb3f42a624
Joey <kovtamas1991@gmail.com>
2017-02-12 14:05:33 +0100

    Mostly implemented CompanyServiceFacade.

--- admin-service/src/main/java/hu/schonherz/project/admin/service/impl/company/CompanyServiceFacade.java
+++ admin-service/src/main/java/hu/schonherz/project/admin/service/impl/company/CompanyServiceFacade.java
@@ -1,35 +1,52 @@
 package hu.schonherz.project.admin.service.impl.company;
 
+import hu.schonherz.project.admin.service.api.service.company.CompanyServiceLocal;
 import hu.schonherz.project.admin.service.api.service.company.CompanyServiceRemote;
 import hu.schonherz.project.admin.service.api.service.company.InvalidCompanyDataException;
 import hu.schonherz.project.admin.service.api.vo.CompanyVo;
 import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.EJBTransactionRolledbackException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class CompanyServiceFacade implements CompanyServiceRemote {
+
+    @EJB
+    private CompanyServiceLocal realService;
 
     @Override
     public CompanyVo save(CompanyVo companyVo) throws InvalidCompanyDataException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            return realService.save(companyVo);
+        } catch (EJBTransactionRolledbackException rolledBackException) {
+            throw new InvalidCompanyDataException("Could not save company: ", rolledBackException);
+        }
     }
 
     @Override
     public CompanyVo findById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return realService.findById(id);
     }
 
     @Override
     public CompanyVo findByName(String companyName) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return realService.findByName(companyName);
     }
 
     @Override
     public List<CompanyVo> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return realService.findAll();
     }
 
     @Override
     public void changeStatus(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        CompanyVo company = realService.findById(id);
+        if (company != null) {
+            // TODO: change status
+        } else {
+            log.warn("The company with id " + id + " does not exist. Cannot change status.");
+        }
     }
 
 }
