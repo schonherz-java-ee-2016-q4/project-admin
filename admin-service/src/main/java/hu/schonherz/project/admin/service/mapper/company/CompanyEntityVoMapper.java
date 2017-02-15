61c6f540bcbe8b08cf4398deb0206135ab7a311a
sutos88 <sutos88@gmail.com>
2017-02-15 12:27:23 +0100

    Company Entity to VO, and VO to Entity added in service

--- admin-service/src/main/java/hu/schonherz/project/admin/service/mapper/company/CompanyEntityVoMapper.java
+++ admin-service/src/main/java/hu/schonherz/project/admin/service/mapper/company/CompanyEntityVoMapper.java
@@ -1,8 +1,15 @@
 package hu.schonherz.project.admin.service.mapper.company;
 
 import hu.schonherz.project.admin.data.entity.CompanyEntity;
+import hu.schonherz.project.admin.data.entity.UserEntity;
+import hu.schonherz.project.admin.data.quota.QuotasEntity;
 import hu.schonherz.project.admin.service.api.vo.CompanyVo;
+import hu.schonherz.project.admin.service.api.vo.QuotasVo;
+import hu.schonherz.project.admin.service.api.vo.UserVo;
+import hu.schonherz.project.admin.service.mapper.user.UserEntityVoMapper;
+
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
@@ -22,16 +29,46 @@ public final class CompanyEntityVoMapper {
         if (entity == null) {
             return null;
         }
-
-        return MAPPER.map(CompanyEntity.class, CompanyVo.class);
+        CompanyVo companyVo = new CompanyVo();
+        QuotasVo quotasVo = new QuotasVo();
+        
+        quotasVo.setMaxUsers(entity.getQuotas().getMaxUsers());
+        quotasVo.setMaxLoggedIn(entity.getQuotas().getMaxLoggedIn());
+        quotasVo.setMaxDayTickets(entity.getQuotas().getMaxDayTickets());
+        quotasVo.setMaxWeekTickets(entity.getQuotas().getMaxWeekTickets());
+        quotasVo.setMaxMonthTickets(entity.getQuotas().getMaxMonthTickets());
+        
+        companyVo.setId(entity.getId());
+        companyVo.setCompanyName(entity.getCompanyName());
+        companyVo.setAdminUser(UserEntityVoMapper.toVo(entity.getAdminUser()));
+        companyVo.setAgents((Set<UserVo>) UserEntityVoMapper.toVo(entity.getAgents()));
+        companyVo.setActive(entity.isActive());
+        companyVo.setQuotes(quotasVo);
+        
+        return companyVo;
     }
 
     public static CompanyEntity toEntity(final CompanyVo vo) {
         if (vo == null) {
             return null;
         }
-
-        return MAPPER.map(CompanyVo.class, CompanyEntity.class);
+        CompanyEntity companyEntity = new CompanyEntity();
+        QuotasEntity quotasEntity = new QuotasEntity();
+        
+        quotasEntity.setMaxUsers(vo.getQuotes().getMaxUsers());
+        quotasEntity.setMaxLoggedIn(vo.getQuotes().getMaxLoggedIn());
+        quotasEntity.setMaxDayTickets(vo.getQuotes().getMaxDayTickets());
+        quotasEntity.setMaxWeekTickets(vo.getQuotes().getMaxWeekTickets());
+        quotasEntity.setMaxMonthTickets(vo.getQuotes().getMaxMonthTickets());
+        
+        companyEntity.setId(vo.getId());
+        companyEntity.setCompanyName(vo.getCompanyName());
+        companyEntity.setAdminUser(UserEntityVoMapper.toEntity(vo.getAdminUser()));
+        companyEntity.setAgents((Set<UserEntity>) UserEntityVoMapper.toEntity(vo.getAgents()));
+        companyEntity.setActive(vo.isActive());
+        companyEntity.setQuotas(quotasEntity);
+        
+        return companyEntity;
     }
 
     public static List<CompanyVo> toVo(final List<CompanyEntity> entities) {
