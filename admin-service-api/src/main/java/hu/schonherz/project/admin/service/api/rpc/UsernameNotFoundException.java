e4fe58dbc69218347b7dd920e68ae33cb718867f
sutos88 <sutos88@gmail.com>
2017-02-20 19:28:41 +0100

    Exception added

---
+++ admin-service-api/src/main/java/hu/schonherz/project/admin/service/api/rpc/UsernameNotFoundException.java
@@ -0,0 +1,14 @@
+package hu.schonherz.project.admin.service.api.rpc;
+
+public class UsernameNotFoundException extends Exception {
+    
+    public UsernameNotFoundException(final String message, final String username) {
+        super(message);
+    }
+
+    public UsernameNotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+
+}
