0f00cae287a3c329c67f354e6dcae5672c3a917a
sutos88 <sutos88@gmail.com>
2017-02-20 23:21:20 +0100

    fixed checkstyle warns

--- admin-service-api/src/main/java/hu/schonherz/project/admin/service/api/rpc/RpcAgentAvailabilityServiceRemote.java
+++ admin-service-api/src/main/java/hu/schonherz/project/admin/service/api/rpc/RpcAgentAvailabilityServiceRemote.java
@@ -2,6 +2,6 @@ package hu.schonherz.project.admin.service.api.rpc;
 
 public interface RpcAgentAvailabilityServiceRemote {
     Long getAvailableAgent(String source) throws NoAvailableAgentFoundException, NoSuchDomainException;
-    
+
     void setAgentAvailabilityToTrue(String username) throws UsernameNotFoundException;
     }
